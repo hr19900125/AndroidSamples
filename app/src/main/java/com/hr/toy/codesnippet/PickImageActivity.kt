8d23189b51e5f2a36d20d90cba1eb49716ca0f8b
黄睿 <huangrui@meizu.com>
2019-07-29 21:14:22 +0800

    add pick image

---
+++ app/src/main/java/com/hr/toy/codesnippet/PickImageActivity.kt
@@ -0,0 +1,25 @@
+package com.hr.toy.codesnippet
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import com.hr.toy.R
+import com.hr.toy.utils.PickImageUtils
+
+class PickImageActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_btn_and_textview)
+        val button: Button = findViewById(R.id.btn)
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = PickImageUtils.getPickImageChooserIntent(this@PickImageActivity, PickImageUtils.ImageSourceType.Gallery)
+                startActivity(intent)
+            }
+        })
+    }
+
+}
